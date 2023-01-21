8244a958b3df3c4df7df272203006683e6170d7b
oreilly.elvis.ricky <oreilly.elvis.ricky@hotmail.com>
2023-01-21 09:26:31 -0500

    ...

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -13,6 +13,12 @@ contract Wallet {
 
   receive() external payable {};
 
+  function mintSmartTokenForToken() {
+  }
+
+  function burnSmartTokenForToken() {
+  }
+
   function withdraw(uint _amount) external {
     require(msg.sender == owner, "caller is not owner");
     payable(msg.sender).transfer(_amount);
